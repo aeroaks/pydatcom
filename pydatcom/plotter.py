862c3618bea58a2cabdd6bf06b89b334fc4c4d09
aeroaks <akshay.tvastr@yahoo.com>
2016-11-14 18:52:46 +0100

    file name correction for windows and python3

--- pydatcom/plotter.py
+++ pydatcom/plotter.py
@@ -21,112 +21,112 @@ class DatcomPlotter(object):
     def common_plots(self):
         ## lift plots
         self.plot2d(
-            title='{name}: Basic Lift Coefficient',
+            title='{name}-Basic Lift Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='CL_Basic', y_label='CL')
         self.plot2d(
-            title='{name}: Flap effect on Lift Coefficient',
+            title='{name}-Flap effect on Lift Coefficient',
             x_name='flap', x_label='Flap, deg',
             y_name='dCL_Flap', y_label='dCL')
         self.plot2d(
-            title='{name}: Elevator effect on Lift Coefficient',
+            title='{name}-Elevator effect on Lift Coefficient',
             x_name='elev', x_label='Elevator, deg',
             y_name='dCL_Elevator', y_label='dCL')
         self.plot2d(
-            title='{name}: Pitch Rate effect on Lift Coefficient',
+            title='{name}-Pitch Rate effect on Lift Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCL_PitchRate', y_label='dCL')
         self.plot2d(
-            title='{name}: Alpha Dot effect on Lift Coefficient',
+            title='{name}-Alpha Dot effect on Lift Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCL_AlphaDot', y_label='dCL')
 
         ## drag plots
         self.plot2d(
-            title='{name}: Basic Drag Coefficient',
+            title='{name}-Basic Drag Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='CD_Basic', y_label='CD')
         self.plot2d(
-            title='{name}: Drag Polar',
+            title='{name}-Drag Polar',
             x_name='CL_Basic', x_label='CL',
             y_name='CD_Basic', y_label='CD')
         self.plot3d(
-            title='{name}: Flap effect on Drag Coefficient',
+            title='{name}-Flap effect on Drag Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='flap', y_label='Flap, deg',
             z_name='dCD_Flap', z_label='dCD')
         self.plot3d(
-            title='{name}: Elevator effect on Drag Coefficient',
+            title='{name}-Elevator effect on Drag Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='elev', y_label='Elevator, deg',
             z_name='dCD_Elevator', z_label='dCD')
 
         ## side force plots
         self.plot2d(
-            title='{name}: Basic Side Force Coefficient',
+            title='{name}-Basic Side Force Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCY_Beta', y_label='dCY')
         self.plot2d(
-            title='{name}: Roll Rate effect on Side Force Coefficient',
+            title='{name}-Roll Rate effect on Side Force Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCY_RollRate', y_label='dCY')
 
         ## roll moment
         self.plot2d(
-            title='{name}: Aileron effect on Roll Moment Coefficient',
+            title='{name}-Aileron effect on Roll Moment Coefficient',
             x_name='alrn', x_label='Aileron, deg',
             y_name='dCl_Aileron', y_label='dCl')
         self.plot2d(
-            title='{name}: Side slip effect on Roll Moment Coefficient',
+            title='{name}-Side slip effect on Roll Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCl_Beta', y_label='dCl')
         self.plot2d(
-            title='{name}: RollRate effect on Roll Moment Coefficient',
+            title='{name}-RollRate effect on Roll Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCl_RollRate', y_label='dCl')
         self.plot2d(
-            title='{name}: YawRate effect on Roll Moment Coefficient',
+            title='{name}-YawRate effect on Roll Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCl_YawRate', y_label='dCl')
 
         ## pitch moment
         self.plot2d(
-            title='{name}: Basic Pitch Moment Coefficient',
+            title='{name}-Basic Pitch Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='Cm_Basic', y_label='Cm')
         self.plot2d(
-            title='{name}: Flap effect on Pitch Moment Coefficient',
+            title='{name}-Flap effect on Pitch Moment Coefficient',
             x_name='flap', x_label='Flap, deg',
             y_name='dCm_Flap', y_label='dCm')
         self.plot2d(
-            title='{name}: Elevator effect on Pitch Moment Coefficient',
+            title='{name}-Elevator effect on Pitch Moment Coefficient',
             x_name='elev', x_label='Elevator, deg',
             y_name='dCm_Elevator', y_label='dCm')
         self.plot2d(
-            title='{name}: Pitch Rate effect on Pitch Moment Coefficient',
+            title='{name}-Pitch Rate effect on Pitch Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCm_PitchRate', y_label='dCm')
         self.plot2d(
-            title='{name}: Alpha Dot effect on Pitch Moment Coefficient',
+            title='{name}-Alpha Dot effect on Pitch Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCm_AlphaDot', y_label='dCm')
 
         ## yaw moment
         self.plot3d(
-            title='{name}: Aileron effect on Yaw Moment Coefficient',
+            title='{name}-Aileron effect on Yaw Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='flap', y_label='Flap, deg',
             z_name='dCn_Aileron', z_label='dCn')
         self.plot2d(
-            title='{name}: Side Slip effect on Yaw Moment Coefficient',
+            title='{name}-Side Slip effect on Yaw Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCn_Beta', y_label='dCn')
         self.plot2d(
-            title='{name}: Roll Rate effect on Yaw Moment Coefficient',
+            title='{name}-Roll Rate effect on Yaw Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCn_RollRate', y_label='dCn')
         self.plot2d(
-            title='{name}: Yaw Rate effect on Yaw Moment Coefficient',
+            title='{name}-Yaw Rate effect on Yaw Moment Coefficient',
             x_name='alpha', x_label='Alpha, deg',
             y_name='dCn_YawRate', y_label='dCn')
 
@@ -171,7 +171,7 @@ class DatcomPlotter(object):
         ax.grid()
         plt.savefig(os.path.join(self.figpath,
             os.path.join(self.figpath,
-                title.format(**self.d) + '.pdf')))
+                title.format(**self.d) + '.png')))
         plt.close(fig)
 
     @staticmethod
--- test/test.py
+++ test/test.py
@@ -11,14 +11,14 @@ class Test(unittest.TestCase):
         pass
 
     def test_parse(self):
-        parser = DatcomParser(os.path.join('test', 'data', 'Citation.out'))
+        parser = DatcomParser(os.path.join(os.path.curdir, 'data', 'Citation.out'))
 
     def test_export(self):
-        parser = DatcomParser(os.path.join('test', 'data', 'Citation.out'))
+        parser = DatcomParser(os.path.join(os.path.curdir, 'data', 'Citation.out'))
         exporter = DatcomExporter(parser.get_common(), 'modelica.mo')
         result = exporter.get_export()
 
     def test_plot(self):
-        parser = DatcomParser(os.path.join('test', 'data', 'Citation.out'))
+        parser = DatcomParser(os.path.join(os.path.curdir, 'data', 'Citation.out'))
         plotter = DatcomPlotter(parser.get_common())
         plotter.common_plots()
